6e7087818579eb52224dd2d0443282556e60043a
Muhammad Ilham Karisma <ilhamsssa@gmail.com>
2023-12-11 15:09:43 +0700

    feat: add whoami function

--- src/route.js
+++ src/route.js
@@ -3,6 +3,18 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   res.send('<h1>Hello I can check your IP Address</h1><form method="get" action="whoami">Are you agree?<input type="submit" value="whoami"/></form>');
-})
+});
+
+router.get('/whoami', (req, res) => {
+  const ipAddress = req.headers['x-forwarded-for'] ? req.headers['x-forwarded-for'].split(',')[0] : req.connection.remoteAddress;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+
+  return res.json({
+    ipaddress: ipAddress,
+    language: language,
+    software: software
+  });
+});
 
 module.exports = router;
